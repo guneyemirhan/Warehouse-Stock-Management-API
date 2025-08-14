fe144da7a934a818327da8cf0e6e34eccec5e747
emirhan güney <emirhnguny1@gmail.com>
2025-08-14 18:39:45 +0300

    feat: Projenin ilk kurulumu ve Warehouse modeli oluşturuldu

---
+++ Downloads/warehouse-stock-management-api/warehouse-stock-management-api/src/main/java/com/project/warehouse_stock_management_api/WarehouseStockManagementApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.warehouse_stock_management_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WarehouseStockManagementApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WarehouseStockManagementApiApplication.class, args);
+	}
+
+}
